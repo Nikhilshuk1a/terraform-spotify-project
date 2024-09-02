64d1ce5f6aca87443eacdaab90bd43f173380820
Nikhilshuk1a <nsnikhil1707@gmail.com>
2024-09-02 20:30:36 +0530

    added comments for better Understanding

--- playlist.tf
+++ playlist.tf
@@ -2,14 +2,17 @@ resource "spotify_playlist" "Playlist_created_using_Terraform" {
     name = "Playlist created using Terraform"
     tracks = ["3e7sxremeOE3wTySiOhGiP","0dJacN6K3mSGLE9p7QE0lO","1DIXPcTDzTj8ZMHt3PDt8p","6mt8Bt9aHHCfbTBJMAcceJ","08bNPGLD8AhKpnnERrAc6G"]
 }
+# Make sure you copy track id from spotify which looks like "https://open.spotify.com/track/<Your Track ID>"
+# If you use track ID which looks like https://open.spotify.com/playlist/<Track ID> OR https://open.spotify.com/album/<Track ID> 
+# This will not Add your Song to your Playlist because type is different, it should be Track.
 
 data "spotify_search_track" "KK" {
     artist = "KK"
-}
+} # searching tracks based on the artist
 
 resource "spotify_playlist" "Songs_by_kk" {
     name = "Best Songs of KK"
     tracks = [data.spotify_search_track.KK.tracks[0].id,data.spotify_search_track.KK.tracks[1].id,
     data.spotify_search_track.KK.tracks[2].id,data.spotify_search_track.KK.tracks[3].id,
     data.spotify_search_track.KK.tracks[4].id]
-}
+} # using data block to provide songs of an artist, used here in tracks in the form of array
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,3 @@
 api_key = "zSCxDC8FjnXqCQyiXLRDlMpJpMiGQ0G9VFjxtfwr1E2lI0WZ1wc7wWf4QJdj6M8U"
+# you will get api_key when you will run a docker container if it is not printed on the terminal(in my case when using after time) 
+# use docker command to get logs of a conatiner "docker logs containerID"
